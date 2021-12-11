2305db75c33937f782c31b5b6b088c78e29e5284
gr997570 <84570715+gr997570@users.noreply.github.com>
2021-12-11 14:11:59 -0400

    Update registration.component.ts

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -47,7 +47,7 @@ export class RegistrationComponent implements OnInit {
                   question: this.loginForm.controls['question'].value,
                   answer: this.loginForm.controls['answer'].value
                 }
-    this.http.post("https://csci5410-g11.herokuapp.com/register", user).subscribe((result:any)=>{
+    this.http.post("https://us-central1-csci5410-334019.cloudfunctions.net/user/register", user).subscribe((result:any)=>{
       if(result.success){
         this.success = true;
         alert("Registration Successful")
